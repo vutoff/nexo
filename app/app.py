b7d9e52fc63262cc5d2eb1455593991ca39fc00d
Iliyan Vutov <iliyan@receipt-bank.com>
2022-10-03 12:08:15 +0300

    Provision MySQL server and connect the application to it
    The MySQL server runs as Deployment. The application simply connects to
    it and returns connection status.

--- app/app.py
+++ app/app.py
@@ -1,11 +1,37 @@
+import os
+
+import mysql.connector
 from flask import Flask
+from mysql.connector import Error
 
 app = Flask(__name__)
+
+MYSQL_HOST = os.environ.get("MYSQL_HOST", "127.0.0.1")
+MYSQL_DATABASE = os.environ.get("MYSQL_DATABASE", "mysql")
+MYSQL_USER = os.environ.get("MYSQL_USER", "root")
+MYSQL_PASSWORD = os.environ.get("MYSQL_PASSWORD", "")
 
 
 @app.route("/")
 def index():
-    return "Hellow world!"
+    message = ""
+    try:
+        connection = mysql.connector.connect(
+            host=MYSQL_HOST, database=MYSQL_DATABASE, user=MYSQL_USER, password=MYSQL_PASSWORD
+        )
+
+        if connection.is_connected():
+            db_Info = connection.get_server_info()
+            message = f"Connected to MySQL Server version {db_Info}\n"
+            cursor = connection.cursor()
+            cursor.execute("SELECT database();")
+            record = cursor.fetchone()
+            message += f"Connected to database: {record}\n"
+
+    except Error as e:
+        message = f"Error connecting to MySQL: {e}"
+
+    return message
 
 
 if __name__ == "__main__":
